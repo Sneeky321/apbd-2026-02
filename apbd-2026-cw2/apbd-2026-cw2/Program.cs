c11f6f7909cc0d9e27eacbff363aa96f77b9706f
Sneeky321 <mikolaj.kur123@gmail.com>
2026-03-12 13:07:53 +0100

    CalculateMax FeatureMax

--- apbd-2026-cw2/apbd-2026-cw2/Program.cs
+++ apbd-2026-cw2/apbd-2026-cw2/Program.cs
@@ -4,3 +4,20 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Feature1");
 Console.WriteLine("Feature2");
 Console.WriteLine("Hi");
+int[] array = { 12, 53, 69, 22, 329 };
+Console.WriteLine(CalculateMax(array));
+
+
+static double CalculateMax(int[] values)
+{
+    int max = values[0];
+    
+    for (int i = 0; i < values.Length; i++)
+    {
+        if (values[i] > max)
+        {
+            max = values[i];
+        }
+    }
+    return max;
+}
